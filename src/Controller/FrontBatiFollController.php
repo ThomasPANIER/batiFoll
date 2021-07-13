c9708a55409424706c3a310144ee1a658bd48a2a
unknown <thomaspanier.pro@gmail.com>
2021-07-13 17:03:32 +0200

    templating ok

--- src/Controller/FrontBatiFollController.php
+++ src/Controller/FrontBatiFollController.php
@@ -3,8 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * Require ROLE_ADMIN for *every* controller method in this class.
+ *
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
+
 
 class FrontBatiFollController extends AbstractController
 {
